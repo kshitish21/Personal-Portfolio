769006c2c4272f61fe41bcdbf25daed73bd123e7
Sanjana1021 <sanjanathammaiah2197.com>
2025-01-03 03:48:19 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "Kshitish";
+const meta = {
+    title: "Kshitish Kumar Moharana",
+    description: "I’m Kshitish Kumar Moharana data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Kshitish Kumar Moharana",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop websites",
+    },
+    description: "Hi, I’m Kshitish, a passionate software developer eager to apply my skills in building innovative digital solutions. With a strong foundation in programming and problem-solving, I’m excited to take on new challenges and contribute to impactful projects. Check out my work, and let's collaborate to bring your ideas to life!",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A Bit About Myself",
+    aboutme: "A passionate software developer eager to create efficient, user-friendly solutions. With a strong foundation in coding and a love for learning, I’m ready to take on new challenges and bring ideas to life. Let’s collaborate and build something impactful together!",
+};
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+},
+];
+
+const skills = [{
+    name: "Java",
+    value: 90,
+},
+{
+    name: "Data Structure",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 60,
+},
+{
+    name: "HTML",
+    value: 90,
+},
+{
+    name: "CSS",
+    value: 85,
+},
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "Crafting intuitive and visually appealing user interfaces that provide seamless user experiences. I focus on design that not only looks great but also enhances usability, ensuring your users enjoy every interaction. Let’s create designs that engage and convert.",
+},
+{
+    title: "Website Development",
+    description: "Specializing in creating responsive, user-friendly websites tailored to your needs. From sleek designs to seamless functionality, I bring your online presence to life with clean code and modern technologies. Let's build a website that stands out and drives results.",
+},
+{
+    title: "Full-Stack Web Development",
+    description: "Providing end-to-end full-stack web development services, from intuitive front-end designs to robust back-end systems. I create scalable, efficient, and dynamic web applications tailored to meet your business needs and deliver seamless user experiences.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kshitish.kumar1231@gmail.com",
+    YOUR_FONE: "+91 6370006338",
+    description: "Interested in collaborating or have any questions? Feel free to get in touch! I’d love to hear from you and discuss how we can work together on exciting projects.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_nug99do",
+    YOUR_TEMPLATE_ID: "template_rmons3f",
+    YOUR_USER_ID: "eG_hNmFk8z1f94Baw",
+};
+
+const socialprofils = {
+    github: "https://github.com/",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
